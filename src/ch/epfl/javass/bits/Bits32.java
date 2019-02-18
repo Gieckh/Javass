5cf1927ae2f945e343ebba173228276018a3f68d
Gieckh <marin.nguyen@gmail.com>
2019-02-18 20:12:43 +0100

    javadoc-ing

--- src/ch/epfl/javass/bits/Bits32.java
+++ src/ch/epfl/javass/bits/Bits32.java
@@ -1,11 +1,14 @@
 package ch.epfl.javass.bits;
 
+//TODO: Junit tests
 public class Bits32 {
     /** ============================================== **/
     /** ==============   CONSTRUCTEURS   ============= **/
-    /** ============================================== **/
-    private Bits32() {}
-
+    /**
+     * ==============================================
+     **/
+    private Bits32() {
+    }
 
     /** ============================================== **/
     /** ===============    METHODES    =============== **/
@@ -13,21 +16,22 @@ public class Bits32 {
 
     /**
      * @brief creates a mask of "size" 1 bits, starting at position "start"
-     *        ex : mask(1, 3) = 0...01110
+     * ex : mask(1, 3) = 0...01110
      * @param start (int) the position of the first 1
-     * @param size (int) the length of the 1-sequence
+     * @param size  (int) the length of the 1-sequence
      * @return (int) the mask
      * @throws IllegalArgumentException
+     *
      * @author - Marin Nguyen (288260)
      */
-    public static int
-    mask (int start, int size) throws IllegalArgumentException {
+    public static int mask(int start, int size) throws IllegalArgumentException {
         if (start < 0 || size < 0) {
             throw new IllegalArgumentException();
         }
         if (start + size >= Integer.SIZE) { //The bits are numbered from 0 to Integer.SIZE - 1 (== 31)
             throw new IllegalArgumentException();
         }
+
         int mask = 1 << (size) - 1; // (theoretically) also works when size == 31 || size == 0
 
         return mask << start;
@@ -35,50 +39,51 @@ public class Bits32 {
 
     /**
      * @brief creates the int formed from the bits [start] to [start + size - 1] of "bits"
-     * @param bits (int)
+     * @param bits  (int)
      * @param start (int)
-     * @param size (int)
+     * @param size  (int)
      * @return (int) the int formed from the bits [start] to [start + size - 1] of "bits"
      * @throws IllegalArgumentException
+     *
      * @author - Marin Nguyen (288260)
      */
-    public static int
-    extract (int bits, int start, int size)  throws IllegalArgumentException {
+    public static int extract(int bits, int start, int size)
+            throws IllegalArgumentException {
         int mask = mask(start, size);
         return (mask & bits) >>> size;
     }
 
+
+
     /**
-     * @brief concatenates the binary expressions of v1 and v2 (where v2's bits are significant than v1's ones)
-     * @param v1 (int) the first (int) to concatenate
-     * @param s1 (int) the size of v1
-     * @param v2 (int) the second (int) to concatenate
-     * @param s2 (int) the size of v2
-     * @return (int) the number formed by the concatenation of v1 and v2
-     * @throws IllegalArgumentException
+     * @brief finds the position of the MSB of the int value
+     * @see packCheck
+     * @param value the bit whose MSB position we want to know, NOT 0.
+     * @return (int) the MSB position (an int between 0 and 31)
+     *
      * @author - Marin Nguyen (288260)
      */
-    public static int
-    pack(int v1, int s1, int v2, int s2) throws IllegalArgumentException {
-        if (!packCheck(v1, s1)  ||  !packCheck(v2, s2)) {
-            throw new IllegalArgumentException();
+    // finds the msb position of an int, assuming there is one (i.e. value != 0)
+    private static int msbPosition(int value) {
+        int mask = 1 << 30;
+        int msbPos = 31;
+
+        while ((mask & value) != 1) {
+            msbPos--;
         }
 
-        // Seems better (to us) to have a new if statement for that condition
-        if (s1 + s2 > Integer.SIZE) {
-            throw new IllegalArgumentException();
-        }
-
-        return (s2 << v1) + s1;
+        return msbPos;
     }
 
     /**
-     * checks whether "size" and "value" are compatible
+     * @brief checks whether "size" and "value" are compatible
+     * @see pack
      * @param value
      * @param size
      * @return (boolean) true if
-     *         - 1 <= size <= 31
-     *         - and "value" doesn't use more bits than specified by "size"
+     * - 1 <= size <= 31
+     * - and "value" doesn't use more bits than specified by "size"
+     *
      * @author - Marin Nguyen (288260)
      */
     private static boolean packCheck(int value, int size) {
@@ -96,21 +101,42 @@ public class Bits32 {
         return (msbPosition < size); // the MSB of 2**size is the bit at the position (size + 1)
     }
 
+
     /**
-     * @brief finds the position of the MSB of the int value
-     * @param value the bit whose MSB position we want to know, NOT 0.
-     * @return (int) the MSB position (an int between 0 and 31)
+     * @brief concatenates the binary expressions of v1 and v2 (where v2's bits are significant than v1's ones)
+     * @param v1 (int) the first (int) to concatenate
+     * @param s1 (int) the size of v1
+     * @param v2 (int) the second (int) to concatenate
+     * @param s2 (int) the size of v2
+     * @return (int) the number formed by the concatenation of v1 and v2
+     * @throws IllegalArgumentException
+     *
      * @author - Marin Nguyen (288260)
      */
-    // finds the msb position of an int, assuming there is one (i.e. value != 0)
-    private static int msbPosition (int value) {
-        int mask = 1 << 30;
-        int msbPos = 31;
-
-        while ((mask & value) != 1) {
-            msbPos--;
+    public static int
+    pack(int v1, int s1, int v2, int s2) throws IllegalArgumentException {
+        if (!packCheck(v1, s1) || !packCheck(v2, s2)) {
+            throw new IllegalArgumentException();
         }
 
-        return msbPos;
+        // Seems better (to us) to have a new if statement for that condition
+        if (s1 + s2 > Integer.SIZE) {
+            throw new IllegalArgumentException();
+        }
+
+        return (s2 << v1) + s1;
+    }
+
+    public static int
+    pack(int v1, int s1, int v2, int s2, int v3, int s3) throws IllegalArgumentException {
+
+        return 0;
+    }
+
+
+    public static int
+    pack(int v1, int s1, int v2, int s2, int v3, int s3, int v4, int s4,
+         int v5, int s5, int v6, int s6, int v7, int s7) throws IllegalArgumentException {
+        return 0;
     }
 }
--- src/ch/epfl/javass/jass/PackedCard.java
+++ src/ch/epfl/javass/jass/PackedCard.java
@@ -5,31 +5,31 @@ public final class PackedCard {
     
     
     public static boolean isValid(int pkCard) {
-        
+        return false;
     }
     
     public static int pack(Card.Color c, Card.Rank r) {
-        
+        return 0;
     }
     
     public static Card.Color color(int pkCard) {
-        
+        return null;
     }
     
     public static Card.Rank rank(int pkCard) {
-        
+        return null;
     }
     
     public static boolean isBetter (Card.Color trump, int pkCardL, int pkCardR) {
-        
+        return false;
     }
     
     public static int points(Card.Color trump, int pkCard) {
-        
+        return 0;
     }
     
     public static String toString(int pkCard) {
-        
+        return "";
     }
     
 }
