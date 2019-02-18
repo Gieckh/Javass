b9283dc0c7a6721f856f1f3c389c620209c2becd
Gieckh <marin.nguyen@gmail.com>
2019-02-18 16:25:22 +0100

    ok

--- src/ch/epfl/javass/Preconditions.java
+++ src/ch/epfl/javass/Preconditions.java
@@ -4,6 +4,12 @@ public final class Preconditions {
     private Preconditions() {}
     // ...méthodes
 
+    /**
+     * (@brief)
+     * @param b
+     * @return none
+     * @throws IllegalArgumentException if b is false
+     */
     public static void checkArgument(boolean b) {
         if (!b) {
             throw new IllegalArgumentException();
@@ -11,6 +17,14 @@ public final class Preconditions {
         // ne fait rien sinon
     }
 
+    /**
+     *
+     * @param index
+     * @param size
+     * @return
+     * @throws IndexOutOfBoundsException if the index is negative, or
+     *         if the index is greater (or equal) than the size
+     */
     public static int checkIndex(int index, int size) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
