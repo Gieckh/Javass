6a87620fa0ccec0974d08cbf28f551fd62542cf3
AJSVB <45511657+AJSVB@users.noreply.github.com>
2019-02-21 14:51:26 +0100

    2.3 done in jass

--- src/ch/epfl/javass/jass/Jass.java
+++ src/ch/epfl/javass/jass/Jass.java
@@ -12,4 +12,73 @@ public interface Jass {
     public final static int WINNING_POINTS = 1000;
     public final static int MATCH_ADDITIONAL_POINTS = 100;
     public final static int LAST_TRICK_ADDITIONAL_POINTS = 5;
+
+    public enum TeamId {
+        TEAM_1  (1), 
+        TEAM_2  (2); 
+ 
+
+        public final int type;
+        public final static int COUNT = 2;
+        public final static List<Color> ALL =
+          Collections.unmodifiableList(Arrays.asList(values()));
+     // TODO : inutile pour le moment ( et pas demandé)
+        TeamId(int type) {
+            this.type = type;
+        }
+
+        /** returns the other team than the one we apply the function on.
+         * @return the other team than the one we apply the function on
+         * @throws IllegalArgumentException
+         * @author Antoine Scardigli - (299905)
+         * @author Marin Nguyen - (288260)
+         */
+        public static TeamId other() throws IllegalArgumentException{
+               switch (this) {
+                case TEAM_1:
+                    return TEAM_2;
+                case TEAM_2:
+                    return TEAM_1;
+                default:
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
+    
+    
+    public enum PlayerId {
+        PLAYER_1  (1), 
+        PLAYER_2  (2); 
+        PLAYER_3  (3);
+        PLAYER_4  (4);
+ 
+        public final int type;
+        public final static int COUNT = 4;
+        public final static List<Color> ALL =
+          Collections.unmodifiableList(Arrays.asList(values()));
+
+        // TODO : inutile pour le moment ( et pas demandé)
+        PlayerId(int type) {
+            this.type = type;
+        }
+
+        /** returns team 1 for players 1 and 3, and team 2 for players 2 and 4.
+         * @return the team in function of the player
+         * @throws IllegalArgumentException
+         * @author Antoine Scardigli - (299905)
+         * @author Marin Nguyen - (288260)
+         */
+        public static TeamId team() throws IllegalArgumentException{
+               switch (this) {
+                   case PLAYER_1:
+                   case PLAYER_3:
+                       return TEAM_1;
+                   case PLAYER_2:
+                   case PLAYER_4:
+                       return TEAM_2;
+                   default:
+                       throw new IllegalArgumentException();
+                }
+            }
+        }
 }
