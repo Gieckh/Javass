5b065f318e157b319db7d41ee74b5c141f028240
Gieckh <marin.nguyen@gmail.com>
2019-02-22 22:46:22 +0100

    2 good tests created

--- test/ch/epfl/javass/jass/PackedScoreTest.java
+++ test/ch/epfl/javass/jass/PackedScoreTest.java
@@ -17,14 +17,25 @@ class PackedScoreTest {
     private final static int MAX_POINTS_PER_TURN = 257;
     private final static int MAX_POINTS_PER_GAME = 2000;
 
-    @Test //Runs in around 42s.
+    @Test //Runs in around 8s. (for around 15.5M "pack" calls)
     void isValidReturnsTrueWithAllValidTricks() { //Symmetric
+        SplittableRandom rng = newRandom();
+        int j1Step = rng.nextInt(1, 150);
+        int j2Step = rng.nextInt(1, 150);
+        int k1Step = rng.nextInt(20, 150);
+        int k2Step = rng.nextInt(20, 150);
+
+        System.out.println("j1Step : " + j1Step);
+        System.out.println("j2Step : " + j2Step);
+        System.out.println("k1Step : " + k1Step);
+        System.out.println("k2Step : " + k2Step);
+
         for (int i1 = 0 ; i1 <= MAX_TRICKS_PER_TURN ; ++i1) {
-            for (int j1 = 0 ; j1 <= MAX_POINTS_PER_TURN ; j1+=17) {
-                for(int k1 = 0 ; k1 <= MAX_POINTS_PER_GAME ; k1+=68) {
+            for (int j1 = 0 ; j1 <= MAX_POINTS_PER_TURN ; j1+=j1Step) {
+                for(int k1 = 0 ; k1 <= MAX_POINTS_PER_GAME ; k1+=k1Step) {
                     for (int i2 = 0 ; i2 <= MAX_TRICKS_PER_TURN; ++i2) {
-                        for(int j2 = 0 ; j2 <= MAX_POINTS_PER_TURN ; j2 += 13) {
-                            for(int k2 = 0; k2 <= MAX_POINTS_PER_GAME ; k2 += 77) {
+                        for(int j2 = 0 ; j2 <= MAX_POINTS_PER_TURN ; j2 += j2Step) {
+                            for(int k2 = 0; k2 <= MAX_POINTS_PER_GAME ; k2 += k2Step) {
                                 long packed1 = pack(i1, j1, k1, i2, j2, k2);
 
                                 //Extreme cases for j's
@@ -48,6 +59,48 @@ class PackedScoreTest {
         }
     }
 
+
+    @Test //Runs in around 5s (for around 4.8M "pack" calls)
+    void isValidReturnsTrueWithAllValidPointsPerTurn() { //Symmetric
+        SplittableRandom rng = newRandom();
+        int i1Step = rng.nextInt(2, MAX_TRICKS_PER_TURN + 1);
+        int i2Step = rng.nextInt(2, MAX_TRICKS_PER_TURN + 1);
+        int k1Step = rng.nextInt(200, 1000);
+        int k2Step = rng.nextInt(180, 1000);
+
+        System.out.println("i1Step : " + i1Step);
+        System.out.println("i2Step : " + i2Step);
+        System.out.println("k1Step : " + k1Step);
+        System.out.println("k2Step : " + k2Step);
+
+        for (int i1 = 0 ; i1 <= MAX_TRICKS_PER_TURN ; i1 += i1Step) {
+            for (int j1 = 0 ; j1 <= MAX_POINTS_PER_TURN ; ++j1) {
+                for(int k1 = 0 ; k1 <= MAX_POINTS_PER_GAME ; k1 += k1Step) {
+                    for (int i2 = 0 ; i2 <= MAX_TRICKS_PER_TURN; i2 += i2Step) {
+                        for(int j2 = 0 ; j2 <= MAX_POINTS_PER_TURN ; ++j2) {
+                            for(int k2 = 0; k2 <= MAX_POINTS_PER_GAME ; k2 += k2Step) {
+                                long packed1 = pack(i1, j1, k1, i2, j2, k2);
+
+                                //Extreme cases for i's
+                                long packed2 = pack(MAX_TRICKS_PER_TURN, j1, k1, i2, j2, k2);
+                                long packed3 = pack(i1, j1, k1, MAX_TRICKS_PER_TURN, j2, k2);
+
+                                //Extreme cases for k's
+                                long packed4 = pack(i1, j1, MAX_POINTS_PER_GAME, i2, j2, k2);
+                                long packed5 = pack(i1, j1, k1, i2, j2, MAX_POINTS_PER_GAME);
+
+                                assertTrue(isValid(packed1));
+                                assertTrue(isValid(packed2));
+                                assertTrue(isValid(packed3));
+                                assertTrue(isValid(packed4));
+                                assertTrue(isValid(packed5));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     // TODO: modify
     @Test
