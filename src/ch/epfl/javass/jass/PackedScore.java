8a28f9a80946d435c44ff4660bd5bb9b20a551f9
Gieckh <marin.nguyen@gmail.com>
2019-02-22 19:13:10 +0100

    ?

--- src/ch/epfl/javass/jass/PackedScore.java
+++ src/ch/epfl/javass/jass/PackedScore.java
@@ -1,11 +1,12 @@
 package ch.epfl.javass.jass;
 
 import ch.epfl.javass.bits.Bits64;
-import ch.epfl.javass.jass.TeamId;
 
 import static ch.epfl.javass.bits.Bits64.extract;
 
-/** manipulates scores of a jass game
+/**
+ * manipulates scores of a jass game
+ *
  * @author Antoine Scardigli - (299905)
  * @author Marin Nguyen - (288260)
  *
@@ -14,9 +15,9 @@ public final class PackedScore {
 
     public final static long INITIAL = 0;
 
-    private final static int ONE_TEAM_SIZE = 32;
+    private final static int TEAM_INFO_SIZE = 32;
     private final static int TEAM_ONE_START = 0;
-    private final static int TEAM_TWO_START = ONE_TEAM_SIZE;
+    private final static int TEAM_TWO_START = TEAM_INFO_SIZE;
 
     private final static int TRICKS_START = 0;
     private final static int TRICKS_SIZE = 4;
@@ -39,41 +40,51 @@ public final class PackedScore {
     //TODO do same for some other classes
     private PackedScore() {};
 
-    /** returns true if pkScore is packed correctly.
+    /**
+     * returns true if pkScore is packed correctly.
+     *
      * @param  pkScore (long) the long encoding the points and tricks of the game
      * @return true if pkScore is packed correctly
+     *
      * @author Antoine Scardigli - (299905)
      * @author Marin Nguyen - (288260)
     */
     public static boolean isValid(long pkScore) {
 
        return(
-              extract(pkScore, TRICKS_START, TRICKS_SIZE) <= MAX_TRICKS_PER_TURN && // the number of tricks is valid (<10)
-              extract(pkScore, POINTS_PER_TURN_START, POINTS_PER_TURN_SIZE) <= MAX_POINTS_PER_TURN && // the number of points per turn is valid (<258)
-              extract(pkScore, POINTS_PER_GAME_START, POINTS_PER_GAME_SIZE + EMPTY_BIT_SIZE) <= MAX_POINTS_PER_GAME && // the number of points of the game is valid (<2000)
+              //TODO: suppr coms on the right ?
+              extract(pkScore, TRICKS_START, TRICKS_SIZE) <= MAX_TRICKS_PER_TURN && // number of tricks is valid (<=10)
+              extract(pkScore, POINTS_PER_TURN_START, POINTS_PER_TURN_SIZE) <= MAX_POINTS_PER_TURN && // number of points per turn is valid (<258)
+              extract(pkScore, POINTS_PER_GAME_START, POINTS_PER_GAME_SIZE + EMPTY_BIT_SIZE) <= MAX_POINTS_PER_GAME && // number of points of the game is valid (<=2000)
               //We want only 0 from the 24th to the 31th bit thus the "+ EMPTY_BIT_SIZE"
-              extract(pkScore, ONE_TEAM_SIZE, TRICKS_SIZE) < MAX_TRICKS_PER_TURN && // the number of tricks is valid (<10)
-              extract(pkScore, TEAM_TWO_START + TRICKS_SIZE, POINTS_PER_TURN_SIZE) < MAX_POINTS_PER_TURN && // the number of points per turn is valid (<258)
-              extract(pkScore, TEAM_TWO_START + POINTS_PER_GAME_START, POINTS_PER_GAME_SIZE + EMPTY_BIT_SIZE) <= MAX_POINTS_PER_GAME ); // the number of points of the game is valid (<2000)
+              extract(pkScore, TEAM_INFO_SIZE, TRICKS_SIZE) <= MAX_TRICKS_PER_TURN && // number of tricks is valid (<=9)
+              extract(pkScore, TEAM_TWO_START + POINTS_PER_TURN_START, POINTS_PER_TURN_SIZE) <= MAX_POINTS_PER_TURN && // number of points per turn is valid (<258)
+              extract(pkScore, TEAM_TWO_START + POINTS_PER_GAME_START, POINTS_PER_GAME_SIZE + EMPTY_BIT_SIZE) <= MAX_POINTS_PER_GAME ); // number of points of the game is valid (<=000)
               //We want only 0 from the 56th to the 63th bit thus the "+ EMPTY_BIT_SIZE"
     }
 
-    /** returns the packed long with all of these informations packed on 64bits.
+    /**
+     * returns the packed long with all of these information packed on 64bits.
+     *
      * @param turnTricks1 (team1)
      * @param turnPoints1 (team1)
      * @param gamePoints1 (team1)
      * @param turnTricks2 (team2)
      * @param turnPoints2 (team2)
      * @param gamePoints2 (team2)
-     * @return the packed long with all of these informations packed on 64bits
+     * @return the packed long with all of these information packed on 64bits
+     *
      * @author Antoine Scardigli - (299905)
      * @author Marin Nguyen - (288260)
     */
-    public static long pack //TODO: pas comme Antoine a fait.
-    (int turnTricks1, int turnPoints1, int gamePoints1, int turnTricks2, int turnPoints2, int gamePoints2) {
-        long lsb= Bits64.pack(turnTricks1, TRICKS_SIZE, turnPoints1, POINTS_PER_TURN_SIZE, gamePoints1, POINTS_PER_GAME_SIZE);
-        long msb= Bits64.pack(turnTricks2, TRICKS_SIZE, turnPoints2, POINTS_PER_TURN_SIZE, gamePoints2, POINTS_PER_GAME_SIZE);
-        return Bits64.pack(lsb, ONE_TEAM_SIZE, msb, ONE_TEAM_SIZE);
+    public static long //TODO: pas comme Antoine a fait - Sami Abuzakuk
+    pack (int turnTricks1, int turnPoints1, int gamePoints1,
+          int turnTricks2, int turnPoints2, int gamePoints2)
+    {
+        long team1 = Bits64.pack(turnTricks1, TRICKS_SIZE, turnPoints1, POINTS_PER_TURN_SIZE, gamePoints1, POINTS_PER_GAME_SIZE);
+        long team2 = Bits64.pack(turnTricks2, TRICKS_SIZE, turnPoints2, POINTS_PER_TURN_SIZE, gamePoints2, POINTS_PER_GAME_SIZE);
+
+        return Bits64.pack(team1, TEAM_INFO_SIZE, team2, TEAM_INFO_SIZE);
     }
 
 
@@ -93,15 +104,17 @@ public final class PackedScore {
     }
 
 
-    /** returns the number of points won in the current turn in function of the team.
+    /**
+     * returns the number of points won in the current turn in function of the team.
+     *
      * @param pkScore (long) the long encoding the points and tricks of the game
      * @param t, the TeamId
      * @return int : the number of points won in the current turn
-     * @throws IllegalArgumentException
+     *
      * @author Antoine Scardigli - (299905)
      * @author Marin Nguyen - (288260)
     */
-    public static int turnPoints(long pkScore, TeamId t) throws IllegalArgumentException{
+    public static int turnPoints(long pkScore, TeamId t)  {
         assert isValid(pkScore);
 
         int shift = (t == TeamId.TEAM_1) ? TEAM_ONE_START : TEAM_TWO_START;
