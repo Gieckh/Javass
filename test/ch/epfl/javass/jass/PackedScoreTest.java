798808f6385508f789efa0d60fe0abacae61b33a
Gieckh <marin.nguyen@gmail.com>
2019-02-22 17:04:02 +0100

    test test

--- test/ch/epfl/javass/jass/PackedScoreTest.java
+++ test/ch/epfl/javass/jass/PackedScoreTest.java
@@ -1,7 +1,16 @@
 package ch.epfl.javass.jass;
 
+import ch.epfl.javass.Preconditions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PackedScoreTest {
     //TODO: à faire
+    @Test
+    void checkArgumentFailsForFalse() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Preconditions.checkArgument(false);
+        });
+    }
 }
--- test/ch/epfl/javass/jass/ScoreTest.java
+++ test/ch/epfl/javass/jass/ScoreTest.java
@@ -1,7 +1,16 @@
 package ch.epfl.javass.jass;
 
+import ch.epfl.javass.Preconditions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScoreTest {
     //TODO: to do
+    @Test
+    void checkArgumentFailsForFalse() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Preconditions.checkArgument(false);
+        });
+    }
 }
