10ab24701717883f3b398eb56af9c0362f8230b4
Scarigli <antoine.scardigli@epfl.ch>
2019-03-01 15:55:25 +0100

    success test sauf 1

---
+++ test/ch/epfl/javass/jass/TeamIdTest.java
@@ -0,0 +1,16 @@
+package ch.epfl.javass.jass;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public final class TeamIdTest {
+    @Test
+    void teamIdTeamsAreCorrect() throws Exception {
+        TeamId team1 = TeamId.TEAM_1;
+        TeamId team2 = TeamId.TEAM_2;
+        
+        assertEquals(TeamId.TEAM_2, team1.other());
+        assertEquals(TeamId.TEAM_1, team2.other());
+    }
+}
