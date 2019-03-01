0e1b9921d94099895b361de62f352ff9190795ee
Scarigli <antoine.scardigli@epfl.ch>
2019-03-01 15:51:05 +0100

    try hard play hard

--- test/ch/epfl/javass/jass/ScoreTest.java
+++ test/ch/epfl/javass/jass/ScoreTest.java
@@ -151,6 +151,41 @@ class ScoreTest {
             }
         }
     }
+    @Test
+    void withAdditionalTricksFailsWithNegativeValues() {
+        Score s = Score.INITIAL;
+        assertThrows(IllegalArgumentException.class, () ->{
+            s.withAdditionalTrick(TeamId.TEAM_1, -1);
+        });
+    }
+
+    @Test
+    void equalsWorks() {
+        SplittableRandom rng = newRandom();
+        for(int i=0 ; i<RANDOM_ITERATIONS ; ++i) {
+            int tT = rng.nextInt(10);
+            int tP = rng.nextInt(258);
+            int gP = rng.nextInt(2001);
+            Score s1 = Score.ofPacked(Bits64.pack(Bits32.pack(tT, 4, tP, 9, gP, 11), Integer.SIZE, Bits32.pack(tT ,4 , tP, 9, gP, 11), Integer.SIZE));
+            Score s2 = Score.ofPacked(Bits64.pack(Bits32.pack(tT, 4, tP, 9, gP, 11), Integer.SIZE, Bits32.pack(tT ,4 , tP, 9, gP, 11), Integer.SIZE));
+            assertTrue(s1.equals(s2));
+        }
+    }
+    
+    @Test
+    void equalsFails() {
+        SplittableRandom rng = newRandom();
+        for(int i=0 ; i<RANDOM_ITERATIONS ; ++i) {
+            int tT = rng.nextInt(9);
+            int tP = rng.nextInt(258);
+            int gP = rng.nextInt(2001);
+            Score s1 = Score.ofPacked(Bits64.pack(Bits32.pack(tT, 4, tP, 9, gP, 11), Integer.SIZE, Bits32.pack(tT ,4 , tP, 9, gP, 11), Integer.SIZE));
+            Score s2 = Score.ofPacked(Bits64.pack(Bits32.pack(tT+1, 4, tP, 9, gP, 11), Integer.SIZE, Bits32.pack(tT+1 ,4 , tP, 9, gP, 11), Integer.SIZE));
+            assertFalse(s1.equals(s2));
+            assertFalse(s1.equals(new String()));
+        }
+    }
+
 }
 
 
