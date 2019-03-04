a827e5b32801651bd43698a892791cf843f9313e
Scarigli <antoine.scardigli@epfl.ch>
2019-03-04 11:17:05 +0100

    18/18

--- test/ch/epfl/javass/bits/Bits64Test.java
+++ test/ch/epfl/javass/bits/Bits64Test.java
@@ -179,6 +179,168 @@ public class Bits64Test {
             Bits64.pack(0L, 32, 0L, 33);
         });
     }
+   
+        @Test
+        void maskProducesCorrectMasks1() {
+            for (int size = 0; size <= Long.SIZE; ++size) {
+                for (int start = 0; start <= Long.SIZE - size; ++start) {
+                    long m = Bits64.mask(start, size);
+                    assertEquals(size, Long.bitCount(m));
+                    assertEquals(size, Long.bitCount(m >>> start));
+                    if (start + size < Long.SIZE)
+                        assertEquals(0, m >>> (start + size));
+                }
+            }
+        }
+
+        @Test
+        void maskFailsWithNegativeStart2() {
+            SplittableRandom rng = newRandom();
+            for (int i = 0; i < RANDOM_ITERATIONS; ++i) {
+                int start = -(i + 1);
+                int size = rng.nextInt(Long.SIZE + 1);
+                assertThrows(IllegalArgumentException.class, () -> {
+                    Bits64.mask(start, size);
+                });
+            }
+        }
+
+        @Test
+        void maskFailsWithNegativeSize3() {
+            SplittableRandom rng = newRandom();
+            for (int i = 0; i < RANDOM_ITERATIONS; ++i) {
+                int start = rng.nextInt(Long.SIZE + 1);
+                int size = -(i + 1);
+                assertThrows(IllegalArgumentException.class, () -> {
+                    Bits64.mask(start, size);
+                });
+            }
+        }
+
+        @Test
+        void maskFailsWithTooBigSize4() {
+            SplittableRandom rng = newRandom();
+            for (int i = 0; i < RANDOM_ITERATIONS; ++i) {
+                int start = rng.nextInt(Long.SIZE + 1);
+                int size = Long.SIZE - start + rng.nextInt(1, 10);
+                assertThrows(IllegalArgumentException.class, () -> {
+                    Bits64.mask(start, size);
+                });
+            }
+        }
+
+        @Test
+        void extractCanExtractAllBitsInOneGo5() {
+            SplittableRandom rng = newRandom();
+            for (int i = 0; i < RANDOM_ITERATIONS; ++i) {
+                long bits = rng.nextLong();
+                assertEquals(bits, Bits64.extract(bits, 0, Long.SIZE));
+            }
+        }
+
+        @Test
+        void extractCanExtractSubgroupsOfBits6() {
+            SplittableRandom rng = newRandom();
+            for (int i = 0; i < RANDOM_ITERATIONS; ++i) {
+                long bits = rng.nextLong();
+                for (int size = 1; size <= Long.SIZE; size *= 2) {
+                    long reComputedBits = 0;
+                    for (int start = Long.SIZE - size; start >= 0; start -= size)
+                        reComputedBits = (reComputedBits << size) | Bits64.extract(bits, start, size);
+                    assertEquals(bits, reComputedBits);
+                }
+            }
+        }
+
+        @Test
+        void extractFailsWithNegativeStart7() {
+            SplittableRandom rng = newRandom();
+            for (int i = 0; i < RANDOM_ITERATIONS; ++i) {
+                int start = -(i + 1);
+                int size = rng.nextInt(Integer.SIZE + 1);
+                assertThrows(IllegalArgumentException.class, () -> {
+                    Bits64.extract(rng.nextLong(), start, size);
+                });
+            }
+        }
+
+        @Test
+        void extractFailsWithNegativeSize8() {
+            SplittableRandom rng = newRandom();
+            for (int i = 0; i < RANDOM_ITERATIONS; ++i) {
+                int start = rng.nextInt(Long.SIZE + 1);
+                int size = -(i + 1);
+                assertThrows(IllegalArgumentException.class, () -> {
+                    Bits64.extract(rng.nextLong(), start, size);
+                });
+            }
+        }
+
+        @Test
+        void extractFailsWithTooBigSize9() {
+            SplittableRandom rng = newRandom();
+            for (int i = 0; i < RANDOM_ITERATIONS; ++i) {
+                int start = rng.nextInt(Long.SIZE + 1);
+                int size = Long.SIZE - start + rng.nextInt(1, 10);
+                assertThrows(IllegalArgumentException.class, () -> {
+                    Bits64.extract(rng.nextLong(), start, size);
+                });
+            }
+        }
+
+        private int[] getSizes10(SplittableRandom rng, int n) {
+            int[] sizes = new int[n];
+            int remainingBits = Long.SIZE;
+            for (int i = 0; i < n; ++i) {
+                sizes[i] = rng.nextInt(1, remainingBits - (n - 1 - i) + 1);
+                remainingBits -= sizes[i];
+            }
+            return sizes;
+        }
+
+        private long[] getValues11(SplittableRandom rng, int[] sizes) {
+            long[] values = new long[sizes.length];
+            for (int i = 0; i < sizes.length; ++i) {
+                long maxSize = 1L << sizes[i];
+                values[i] = rng.nextLong(maxSize == Long.MIN_VALUE ? Long.MAX_VALUE : maxSize);
+            }
+            return values;
+        }
+
+        @Test
+        void pack2Works12() {
+            SplittableRandom rng = newRandom();
+            for (int i = 0; i < RANDOM_ITERATIONS; ++i) {
+                int[] s = getSizes(rng, 2);
+                long[] v = getValues(rng, s);
+                long packed = Bits64.pack(v[0], s[0], v[1], s[1]);
+                for (int j = 0; j < s.length; ++j) {
+                  assertEquals(v[j], packed & ((1L << s[j]) - 1));
+                  packed >>>= s[j];
+                }
+                assertEquals(0, packed);
+            }
+        }
+
+        @Test
+        void pack2FailsWithTooBigValues13() throws Exception {
+            assertThrows(IllegalArgumentException.class, () -> {
+                Bits64.pack(0b100, 2, 0b11, 2);
+            });
+            assertThrows(IllegalArgumentException.class, () -> {
+                Bits64.pack(0b100, 3, 0b11, 1);
+            });
+        }
+
+        @Test
+        void pack2FailsForTooManyBits14() throws Exception {
+            assertThrows(IllegalArgumentException.class, () -> {
+                Bits64.pack(0, 32, 0, 33);
+            });
+        }
+    }
+
+
 
 //    @Test
 //    void pack3Works() {
@@ -275,4 +437,4 @@ public class Bits64Test {
 //            Bits64.pack(0L, 8, 0L, 8, 0L, 10, 0L, 10, 0L, 10, 0L, 10, 0L, 9 ); //65 bits total
 //        });
 //    }
-}
+
--- test/ch/epfl/javass/jass/TeamIdTest.java
+++ test/ch/epfl/javass/jass/TeamIdTest.java
@@ -1,6 +1,10 @@
 package ch.epfl.javass.jass;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,4 +17,43 @@ public final class TeamIdTest {
         assertEquals(TeamId.TEAM_2, team1.other());
         assertEquals(TeamId.TEAM_1, team2.other());
     }
-}
+   
+
+        private static TeamId[] getAllTeamIds() {
+            return new TeamId[] {
+                    TeamId.TEAM_1,
+                    TeamId.TEAM_2,
+            };
+        }
+
+        @Test
+        void teamIdsAreInRightOrder() {
+            assertArrayEquals(getAllTeamIds(), TeamId.values());
+        }
+
+        @Test
+        void teamIdAllIsCorrect() {
+            assertEquals(Arrays.asList(TeamId.values()), TeamId.ALL);
+        }
+
+        @Test
+        void teamIdAllIsUnmodifiable() {
+            assertThrows(UnsupportedOperationException.class, () -> {
+                TeamId.ALL.set(0, null);
+            });
+        }
+
+        @Test
+        void teamIdCountIsCorrect() {
+            assertEquals(getAllTeamIds().length, TeamId.COUNT);
+        }
+
+        @Test
+        void otherIsCorrect() {
+            assertEquals(TeamId.TEAM_2, TeamId.TEAM_1.other());
+            assertEquals(TeamId.TEAM_1, TeamId.TEAM_2.other());
+        }
+    }
+
+
+
