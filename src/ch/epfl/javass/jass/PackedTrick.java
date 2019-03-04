4719973a09f658381fd6a96ecceaba17893de927
Gieckh <marin.nguyen@gmail.com>
2019-03-04 13:50:36 +0100

    hahaha

--- src/ch/epfl/javass/jass/PackedTrick.java
+++ src/ch/epfl/javass/jass/PackedTrick.java
@@ -4,6 +4,7 @@ public final class PackedTrick {
     /** ============================================== **/
     /** ==============    ATTRIBUTES    ============== **/
     /** ============================================== **/
+    public static final int INVALID = 0b11111111_11111111_11111111_11111111;
 
     /** ============================================== **/
     /** ==============   CONSTRUCTORS   ============== **/
@@ -24,7 +25,59 @@ public final class PackedTrick {
         return 0;
     }
 
-    public int nextEmpty(int pkTrick) {
+    public static int nextEmpty(int pkTrick) {
         return 0;
+    }
+
+    public static boolean isLast(int pkTrick) {
+        return false;
+    }
+
+    public static boolean isEmpty (int pkTrick) {
+        return false;
+    }
+
+    public static boolean isFull(int pkTrick) {
+        return false;
+    }
+
+    public static int size(int pkTrick) {
+        return 0;
+    }
+
+    public static Card.Color trump (int pkTrick) {
+        return null;
+    }
+
+    public static PlayerId player (int pkTrick) {
+        return null;
+    }
+
+    public static int index (int pkTrick) {
+        return 0;
+    }
+
+    public static int card (int pkTrick, int pkCard) {
+        return 0;
+    }
+
+    public static int withAddedCard(int pkTrick, int pkCard) {
+        return 0;
+    }
+
+    public static Card.Color baseColor() {
+        return null;
+    }
+
+    public static CardSet playableCards(CardSet hand) {
+        return null;
+    }
+
+    public static int points(int pkTrick) {
+        return 0;
+    }
+
+    public static PlayerId winningPlayer(int pkTrick) {
+        return null;
     }
 }
