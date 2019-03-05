57a85b7d5e082805e4cfae3724ec6eb06db7c815
Gieckh <marin.nguyen@gmail.com>
2019-03-05 13:26:08 +0100

    Starting tricks

--- test/ch/epfl/javass/jass/PackedTrickTest.java
+++ test/ch/epfl/javass/jass/PackedTrickTest.java
@@ -46,23 +46,25 @@ public class PackedTrickTest {
     void nextEmptyWorks() {
       for (int i = 0 ; i != -1; ++i ) {
           if (PackedTrick.isValid(i)) {
-                       if(Bits32.extract(i, 24, 4) == 8) {
-                           assertEquals(PackedTrick.INVALID, PackedTrick.nextEmpty(i));
-                       }
-                       else {
-                           int nextTrick = PackedTrick.nextEmpty(i);
-                           assertEquals(Bits32.extract(nextTrick, 0, 24), 0b111111111111111111111111);
-                           assertEquals(Bits32.extract(i, 30, 2), Bits32.extract(nextTrick, 30, 2));
-                           assertEquals(PackedTrick.winningPlayer(i).ordinal(), Bits32.extract(nextTrick, 28, 2));
-                           assertEquals(Bits32.extract(i, 24, 4)  , Bits32.extract(nextTrick, 24, 4));
-                           System.out.println("First Player : " + PackedTrick.player(nextTrick, 0));
-                           System.out.println("Card 2 : " + PackedTrick.card(nextTrick, 1));
-                           System.out.println("Card 1 : " + PackedTrick.card(nextTrick, 0));
-                           System.out.println("Card 3 : " + PackedTrick.card(nextTrick, 2));
-                           System.out.println("Card 4 : " + PackedTrick.card(nextTrick, 3));
-                           System.out.println("Winning Player : " + PackedTrick.winningPlayer(nextTrick));
+                   if(Bits32.extract(i, 24, 4) == 8) {
+                       assertEquals(PackedTrick.INVALID, PackedTrick.nextEmpty(i));
+                   }
+                   else {
+                       int nextTrick = PackedTrick.nextEmpty(i);
 
-                    }
+                       System.out.println("First Player : " + PackedTrick.player(nextTrick, 0));
+                       System.out.println("Card 2 : " + PackedTrick.card(nextTrick, 1));
+                       System.out.println("Card 1 : " + PackedTrick.card(nextTrick, 0));
+                       System.out.println("Card 3 : " + PackedTrick.card(nextTrick, 2));
+                       System.out.println("Card 4 : " + PackedTrick.card(nextTrick, 3));
+                       System.out.println("Winning Player : " + PackedTrick.winningPlayer(nextTrick));
+
+                       assertEquals(Bits32.extract(nextTrick, 0, 24), 0b111111111111111111111111);
+                       assertEquals(Bits32.extract(i, 30, 2), Bits32.extract(nextTrick, 30, 2));
+                       assertEquals(PackedTrick.winningPlayer(i).ordinal(), Bits32.extract(nextTrick, 28, 2));
+                       assertEquals(Bits32.extract(i, 24, 4)  , Bits32.extract(nextTrick, 24, 4));
+
+                }
             }
         }
     }
