c91e8d64693e85757a8892ea1e7f38b7db9a525d
Scarigli <antoine.scardigli@epfl.ch>
2019-03-05 14:17:44 +0100

    pulp

--- test/ch/epfl/javass/jass/PackedTrickTest.java
+++ test/ch/epfl/javass/jass/PackedTrickTest.java
@@ -53,9 +53,13 @@ public class PackedTrickTest {
                            int nextTrick = PackedTrick.nextEmpty(i);
                            assertEquals(Bits32.extract(nextTrick, 0, 24), 0b111111111111111111111111);
                            assertEquals(Bits32.extract(i, 30, 2), Bits32.extract(nextTrick, 30, 2));
-                           assertEquals(PackedTrick.winningPlayer(i).ordinal(), Bits32.extract(nextTrick, 28, 2));
+                          System.out.println(i);
+                           System.out.println(PackedTrick.winningPlayer(i) );
+                          System.out.println(PlayerId.ALL.get(Bits32.extract(nextTrick, 28, 2)));
+                           assertEquals(PackedTrick.winningPlayer(i), PlayerId.ALL.get(Bits32.extract(nextTrick, 28, 2)));
                            //System.out.println(Integer.toBinaryString(i));
                            //System.out.println(Integer.toBinaryString(nextTrick));
+                           
                            assertEquals(Bits32.extract(i, 24, 4) +1  , Bits32.extract(nextTrick, 24, 4));
             
                     }
