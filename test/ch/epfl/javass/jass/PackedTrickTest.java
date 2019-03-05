d7fe3ec8a0d3f8427f72b7ef9f471a4b8d47f087
Gieckh <marin.nguyen@gmail.com>
2019-03-05 14:33:09 +0100

    test

--- test/ch/epfl/javass/jass/PackedTrickTest.java
+++ test/ch/epfl/javass/jass/PackedTrickTest.java
@@ -50,12 +50,13 @@ public class PackedTrickTest {
                            assertEquals(PackedTrick.INVALID, PackedTrick.nextEmpty(i));
                        }
                        else {
+                           System.out.println(Integer.toBinaryString(i));
                            int nextTrick = PackedTrick.nextEmpty(i);
                            assertEquals(Bits32.extract(nextTrick, 0, 24), 0b111111111111111111111111);
                            assertEquals(Bits32.extract(i, 30, 2), Bits32.extract(nextTrick, 30, 2));
-                          System.out.println(Integer.toBinaryString(i));
-                          System.out.println(PackedTrick.winningPlayer(i) );
-                          System.out.println(Integer.toBinaryString(nextTrick));
+                          System.out.println("i : " + Integer.toBinaryString(i));
+                          System.out.println("winningPlayer : " + PackedTrick.winningPlayer(i) );
+                          System.out.println("nextTrick : " + Integer.toBinaryString(nextTrick));
                           System.out.println(PlayerId.ALL.get(Bits32.extract(nextTrick, 28, 2)));
                           System.out.println();
                            assertEquals(PackedTrick.winningPlayer(i), PlayerId.ALL.get(Bits32.extract(nextTrick, 28, 2)));
