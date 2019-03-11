5954b14241813f0e20916d5db84e211114a20a66
Scarigli <antoine.scardigli@epfl.ch>
2019-03-11 14:57:13 +0100

    1 class finie

--- src/ch/epfl/javass/jass/TurnState.java
+++ src/ch/epfl/javass/jass/TurnState.java
@@ -1,70 +1,113 @@
 package ch.epfl.javass.jass;
 
 public final class TurnState {
+    
     /** ============================================== **/
     /** ==============    ATTRIBUTES    ============== **/
     /** ============================================== **/
+    
+    private final long pkScore;
+    private final long pkUnplayedCards;
+    private final int pkCurrentTrick;
 
     /** ============================================== **/
     /** ==============   CONSTRUCTORS   ============== **/
     /** ============================================== **/
-    private TurnState() {
-
+  //  private TurnState() {
+     //}
+    
+    private TurnState(long pkScore, long pkUnplayedCards, int pkCurrentTrick) {
+        this.pkScore = pkScore;
+        this.pkUnplayedCards = pkUnplayedCards;
+        this.pkCurrentTrick = pkCurrentTrick;
     }
 
     /** ============================================== **/
     /** ===============    METHODS    ================ **/
     /** ============================================== **/
     public static TurnState initial(Card.Color trump, Score score, PlayerId firstPlayer) {
-
+        return new TurnState(score.packed(), PackedCardSet.ALL_CARDS, PackedTrick.firstEmpty(trump, firstPlayer)); 
     }
 
-    public static TurnState ofPackedComponents(long pkScore, long pkUnplayedCards, int pkTrick) {
-
+    public static TurnState ofPackedComponents(long pkScore, long pkUnplayedCards, int pkCurrentTrick) {
+        if(PackedScore.isValid(pkScore)&&PackedCardSet.isValid(pkUnplayedCards)&&PackedTrick.isValid(pkCurrentTrick)) {
+            return new TurnState(pkScore, pkUnplayedCards, pkCurrentTrick);
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+        
     }
 
     public long packedScore() {
-
+        return this.pkScore;
     }
 
     public long packedUnplayedCards() {
-
+        return this.pkUnplayedCards;
     }
 
     public int packedTrick() {
-
+        return this.pkCurrentTrick;
     }
 
     public Score score() {
-
+        return Score.ofPacked(pkScore);
     }
 
     public CardSet unplayedCards() {
-
+        return CardSet.ofPacked(pkScore);
     }
 
     public Trick trick() {
-
+        return Trick.ofPacked(pkCurrentTrick);
     }
 
 
     public boolean isTerminal() {
-
+        return (PackedTrick.nextEmpty(pkCurrentTrick)==PackedTrick.INVALID);
     }
 
     public PlayerId nextPlayer() {
-
+        if(PackedTrick.isFull(pkCurrentTrick)) {
+            throw new IllegalStateException();
+        }
+        else {
+            return PackedTrick.player(pkCurrentTrick, PackedTrick.size(pkCurrentTrick));
+        }
+         
     }
 
     public TurnState withNewCardPlayed(Card card) {
-
+        if(PackedTrick.isFull(pkCurrentTrick)) {
+            throw new IllegalStateException();
+        }
+        else {
+            return new TurnState(pkScore, PackedCardSet.remove(pkUnplayedCards, card.packed()),PackedTrick.withAddedCard(pkCurrentTrick, card.packed()));
+        }
     }
 
     public TurnState withTrickCollected() {
-
+        if(!PackedTrick.isFull(pkCurrentTrick)) {
+            throw new IllegalStateException();
+        }
+        else {
+            return new TurnState(PackedScore.nextTurn(pkScore), pkUnplayedCards , pkCurrentTrick);
+        }
+        
     }
 
     public TurnState withNewCardPlayedAndTrickCollected(Card card) {
-
+        if(PackedTrick.isFull(pkCurrentTrick)) {
+            throw new IllegalStateException();
+        }
+        else {
+            if(!PackedTrick.isFull(pkCurrentTrick)) {
+                throw new IllegalStateException();
+            }
+            else {
+                return new TurnState(PackedScore.nextTurn(pkScore), PackedCardSet.remove(pkUnplayedCards, card.packed()),PackedTrick.withAddedCard(pkCurrentTrick, card.packed()));
+            }
+        }
     }
 }
