f98fdd1fb0f7b78a07e839e9043b6efa45ac0304
Gieckh <marin.nguyen@gmail.com>
2019-03-11 13:31:48 +0100

    Interface player

---
+++ src/ch/epfl/javass/jass/Player.java
@@ -0,0 +1,31 @@
+package ch.epfl.javass.jass;
+
+import java.util.Map;
+
+public interface Player {
+    abstract public Card cardToPlay(TurnState state, CardSet hand);
+
+    default public void setPlayers(PlayerId ownId, Map<PlayerId, String> playerNames) {
+        //default is empty
+    }
+
+    default public void updateHand(CardSet newHand) {
+        //default is empty
+    }
+
+    default public void setTrump(Card.Color trump) {
+        //default is empty
+    }
+
+    default public void updateTrick(Trick newTrick) {
+        //default is empty
+    }
+
+    default public void updateScore(Score score) {
+        //default is empty
+    }
+
+    default public void setWinningTeam(TeamId winningTeam) {
+        //default is empty
+    }
+}
