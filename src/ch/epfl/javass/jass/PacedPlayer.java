59fb6cd86bf8f88733d5640e806bcf980ce2a474
Gieckh <marin.nguyen@gmail.com>
2019-03-11 14:36:23 +0100

    PacedPlayer - bis

--- src/ch/epfl/javass/jass/PacedPlayer.java
+++ src/ch/epfl/javass/jass/PacedPlayer.java
@@ -1,18 +1,20 @@
 package ch.epfl.javass.jass;
 
+import java.util.Map;
+
 //TODO: see of the implementation is right
 public final class PacedPlayer implements Player{
     /** ============================================== **/
     /** ==============    ATTRIBUTES    ============== **/
     /** ============================================== **/
-    private Player player;
+    private Player underLyingPlayer;
     private double minTime;
 
     /** ============================================== **/
     /** ==============   CONSTRUCTORS   ============== **/
     /** ============================================== **/
     public PacedPlayer(Player underlyingPlayer, double minTime) {
-        this.player = underlyingPlayer;
+        this.underLyingPlayer = underlyingPlayer;
         this.minTime = minTime;
     }
 
@@ -23,7 +25,7 @@ public final class PacedPlayer implements Player{
     @Override
     public Card cardToPlay(TurnState state, CardSet hand) {
         long tIni = System.currentTimeMillis();
-        Card cardToPlay = player.cardToPlay(state, hand);
+        Card cardToPlay = underLyingPlayer.cardToPlay(state, hand);
         long timeElapsed = (System.currentTimeMillis() - tIni);
         if (timeElapsed < minTime) {
             try {
@@ -32,5 +34,36 @@ public final class PacedPlayer implements Player{
         }
         
         return cardToPlay;
+    }
+
+    @Override
+    public void setPlayers(PlayerId ownId,
+            Map<PlayerId, String> playerNames) {
+        underLyingPlayer.setPlayers(ownId, playerNames);
+    }
+
+    @Override
+    public void updateHand(CardSet newHand) {
+        underLyingPlayer.updateHand(newHand);
+    }
+
+    @Override
+    public void setTrump(Card.Color trump) {
+        underLyingPlayer.setTrump(trump);
+    }
+
+    @Override
+    public void updateTrick(Trick newTrick) {
+        underLyingPlayer.updateTrick(newTrick);
+    }
+
+    @Override
+    public void updateScore(Score score) {
+        underLyingPlayer.updateScore(score);
+    }
+
+    @Override
+    public void setWinningTeam(TeamId winningTeam) {
+        underLyingPlayer.setWinningTeam(winningTeam);
     }
 }
