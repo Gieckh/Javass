d3c5b07e8f97f25cc703ab31ab9ef0575fd7a5c7
Gieckh <marin.nguyen@gmail.com>
2019-03-11 13:27:20 +0100

    TurnState creation

---
+++ src/ch/epfl/javass/jass/TurnState.java
@@ -0,0 +1,70 @@
+package ch.epfl.javass.jass;
+
+public class TurnState {
+    /** ============================================== **/
+    /** ==============    ATTRIBUTES    ============== **/
+    /** ============================================== **/
+
+    /** ============================================== **/
+    /** ==============   CONSTRUCTORS   ============== **/
+    /** ============================================== **/
+    private TurnState() {
+
+    }
+
+    /** ============================================== **/
+    /** ===============    METHODS    ================ **/
+    /** ============================================== **/
+    public static TurnState initial(Card.Color trump, Score score, PlayerId firstPlayer) {
+
+    }
+
+    public static TurnState ofPackedComponents(long pkScore, long pkUnplayedCards, int pkTrick) {
+
+    }
+
+    public long packedScore() {
+
+    }
+
+    public long packedUnplayedCards() {
+
+    }
+
+    public int packedTrick() {
+
+    }
+
+    public Score score() {
+
+    }
+
+    public CardSet unplayedCards() {
+
+    }
+
+    public Trick trick() {
+
+    }
+
+
+    public boolean isTerminal() {
+
+    }
+
+    public PlayerId nextPlayer() {
+
+    }
+
+    public TurnState withNewCardPlayed(Card card) {
+
+    }
+
+    public TurnState withTrickCollected() {
+
+    }
+
+    public TurnState withNewCardPlayedAndTrickCollected(Card card) {
+
+    }
+}
