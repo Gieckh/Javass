1af3a3246e8d77a872d8c6f49940c6b08ef1e5da
Gieckh <marin.nguyen@gmail.com>
2019-03-11 13:40:37 +0100

    nextEmpty

--- src/ch/epfl/javass/jass/PacedPlayer.java
+++ src/ch/epfl/javass/jass/PacedPlayer.java
@@ -1,17 +1,24 @@
 package ch.epfl.javass.jass;
 
-public final class PacedPlayer {
+public final class PacedPlayer implements Player{
     /** ============================================== **/
     /** ==============    ATTRIBUTES    ============== **/
     /** ============================================== **/
+    //private Player player; ?
 
     /** ============================================== **/
     /** ==============   CONSTRUCTORS   ============== **/
     /** ============================================== **/
+    public PacedPlayer(Player underlyingPlayer, double minTime) {
+        //TODO
+    }
+
 
     /** ============================================== **/
     /** ===============    METHODS    ================ **/
     /** ============================================== **/
-
-
+    @Override
+    public Card cardToPlay(TurnState state, CardSet hand) {
+        return null;
+    }
 }
