0a92448fe506ef6c7b43429a3c469994fdd09fa1
Scarigli <antoine.scardigli@epfl.ch>
2019-03-22 15:41:51 +0100

    x

--- src/ch/epfl/javass/jass/JassGame.java
+++ src/ch/epfl/javass/jass/JassGame.java
@@ -117,9 +117,9 @@ public final class JassGame {
             PlayerId playerId = turnState.nextPlayer();
             Player player = players.get(playerId);
             //TODO: suppr sysout ?
-            System.out.println();
-            System.out.println(playerNames.get(playerId) + " is playing ...");
-
+//            System.out.println();
+//            System.out.println(playerNames.get(playerId) + " is playing ...");
+//
             CardSet oldHand = playerHands.get(playerId);
             Card cardToPlay = players.get(playerId).cardToPlay(turnState, oldHand);
             CardSet newHand = oldHand.remove(cardToPlay);
--- src/ch/epfl/javass/jass/RandomJassGame.java
+++ src/ch/epfl/javass/jass/RandomJassGame.java
@@ -11,9 +11,12 @@ public final class RandomJassGame {
       for (PlayerId pId: PlayerId.ALL) {
 //        Player player = new RandomPlayer(2039);
         Player player = new PacedPlayer(new RandomPlayer(2019), 1000);
-//        if (pId == PlayerId.PLAYER_1) {
+       if (pId == PlayerId.PLAYER_1) {
           player = new PacedPlayer(new PrintingPlayer(player), 1000);
-//        }
+        }
+       if (pId == PlayerId.PLAYER_2) {
+           player = new MctsPlayer(PlayerId.PLAYER_2, 2019, 100);
+         }
         players.put(pId, player);
         playerNames.put(pId, pId.name());
       }
