980aadd6777e3ea396098b02f3c846ddc506c083
Gieckh <marin.nguyen@gmail.com>
2019-03-28 19:47:04 +0100

    .

--- debugging/ch/epfl/javass/jass/RandomJassGame3.java
+++ debugging/ch/epfl/javass/jass/RandomJassGame3.java
@@ -9,25 +9,32 @@ public final class RandomJassGame3 {
         Map<PlayerId, Player> players = new HashMap<>();
         Map<PlayerId, String> playerNames = new HashMap<>();
         //marche pour n'importe quelle seed
+        Player player;
         for (PlayerId pId: PlayerId.ALL) {
-            Player player = new MctsPlayer3(pId, 2019, 1000);
-            if (pId.team() == TeamId.TEAM_2) {
+            if (pId.team() == TeamId.TEAM_1)
+                player = new MctsPlayer3(pId, 2019, 10_000);
+            else
                 player = new RandomPlayer(2019);
-            }
-
 
             players.put(pId, player);
             playerNames.put(pId, pId.name());
         }
 
-        for (int k = 2000 ; k < 3000 ; ++k){
-            System.out.println("itération : " + k);
-            JassGame g = new JassGame(k, players, playerNames);
-            while (!g.isGameOver()) {
-                g.advanceToEndOfNextTrick();
-                //                System.out.println(
-                //                        "-------------------------------------------------------------");
-            }
+//        for (int k = 2000 ; k < 3000 ; ++k){
+//            System.out.println("itération : " + k);
+//            JassGame g = new JassGame(k, players, playerNames);
+//            while (!g.isGameOver()) {
+//                g.advanceToEndOfNextTrick();
+//                //                System.out.println(
+//                //                        "-------------------------------------------------------------");
+//            }
+//        }
+
+        JassGame g = new JassGame(2000, players, playerNames);
+        while(!g.isGameOver()) {
+            g.advanceToEndOfNextTrick();
+//            System.out.println("-------------------------------------------------------------");
         }
+        System.out.println("Over");
     }
 }
