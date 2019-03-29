39bd67bc31ae4407df87db1c2ac78ed69248a8c6
Scarigli <antoine.scardigli@epfl.ch>
2019-03-29 12:24:02 +0100

    il seme la mort

--- debugging/ch/epfl/javass/jass/Oktogon.java
+++ debugging/ch/epfl/javass/jass/Oktogon.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 //this is for a test, thus no need of javadoc
 @SuppressWarnings("Duplicates")
-public final class RandomJassGame2 {
+public final class Oktogon {
     public static void main(String[] args) {
         Map<PlayerId, Player> players = new HashMap<>();
         Map<PlayerId, String> playerNames = new HashMap<>();
@@ -13,7 +13,7 @@ public final class RandomJassGame2 {
         for (PlayerId pId: PlayerId.ALL) {
             Player player = new MctsPlayerThomabenmato(pId, 2019, iterations);
             if (pId.team() == TeamId.TEAM_2) {
-                 player = new MctsPlayer3(pId, 2019, iterations);
+                 player = new MctsPlayer(pId, 2019, iterations);
 
             }
 
