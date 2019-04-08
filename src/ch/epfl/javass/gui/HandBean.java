ba7810c39f7d1c3241e385f6c5e5e7070d45e97f
Scarigli <antoine.scardigli@epfl.ch>
2019-04-08 16:19:53 +0200

    .

--- src/ch/epfl/javass/gui/HandBean.java
+++ src/ch/epfl/javass/gui/HandBean.java
@@ -1,9 +1,22 @@
 package ch.epfl.javass.gui;
 
+import static javafx.collections.FXCollections.observableArrayList;
+import static javafx.collections.FXCollections.observableSet;
+
+import com.sun.javafx.UnmodifiableArrayList;
+
+import ch.epfl.javass.jass.Card;
+import ch.epfl.javass.jass.CardSet;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableSet;
+
 public final class HandBean {
     /** ============================================== **/
     /** ==============    ATTRIBUTES    ============== **/
     /** ============================================== **/
+    
+    private ObservableList<Card> hand =  observableArrayList();
+    private ObservableSet<Card> playableCards  = observableSet();
 
     /** ============================================== **/
     /** ==============   CONSTRUCTORS   ============== **/
@@ -12,4 +25,27 @@ public final class HandBean {
     /** ============================================== **/
     /** ===============    METHODS    ================ **/
     /** ============================================== **/
+    
+    public ObservableList<Card> hand() {
+        return hand;
+    }
+    
+//    public void setHand(CardSet newHand) {
+//        newHand.
+//        ObservableList<Card> hand;
+//        for(Card card : newHand) {
+//            
+//        }
+//                if(newHand.size()==9) {
+//            hand = new UnmodifiableObservableList<Card>();
+//        }
+//    }
+    
+    public ObservableSet<Card> playableCards() {
+        return playableCards; 
+    }
+    
+    public void setPlayableCards(CardSet newPlayableCards) {
+        
+    }
 }
