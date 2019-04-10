9391196cd07b54d867b890b50dbe97f54f4c1885
Scarigli <antoine.scardigli@epfl.ch>
2019-04-10 21:46:27 +0200

    ScoreBean JDoced

--- src/ch/epfl/javass/gui/ScoreBean.java
+++ src/ch/epfl/javass/gui/ScoreBean.java
@@ -1,6 +1,6 @@
 package ch.epfl.javass.gui;
 
-import java.beans.Beans;
+import com.sun.xml.internal.fastinfoset.sax.Properties;
 
 import ch.epfl.javass.jass.TeamId;
 import javafx.beans.property.IntegerProperty;
@@ -10,7 +10,22 @@ import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
+/**
+ * ScoreBean is a bean containing the properties of the scores.
+ *
+ *
+ * @author Antoine Scardigli - (299905)
+ * @author Marin Nguyen - (288260)
+ */
+/**
+ * ScoreBean
+ *
+ *
+ * @author Antoine Scardigli - (299905)
+ * @author Marin Nguyen - (288260)
+ */
 public final class ScoreBean {
+    
     /** ============================================== **/
     /** ==============    ATTRIBUTES    ============== **/
     /** ============================================== **/
@@ -28,10 +43,18 @@ public final class ScoreBean {
     /** ==============   CONSTRUCTORS   ============== **/
     /** ============================================== **/
 
+    //default constructor per default is enough
+    
     /** ============================================== **/
     /** ===============    METHODS    ================ **/
     /** ============================================== **/
 
+    /**
+     * @Brief It is a public getter for the turnPoints property of the chosen team.
+     *
+     * @param team the TeamId of a team
+     * @return the turnPoints property of the chosen team
+    */
     public ReadOnlyIntegerProperty turnPointsProperty(TeamId team) {
         if(team.equals(TeamId.TEAM_1)) {
             return turnPointsT1;
@@ -40,6 +63,13 @@ public final class ScoreBean {
             return turnPointsT2;
         }
     }
+    
+    /**
+     * @Brief It is a public getter for the gamePoints property of the chosen team.
+     *
+     * @param team the TeamId of a team
+     * @return the gamePoints property of the chosen team
+    */
     public ReadOnlyIntegerProperty gamePointsProperty(TeamId team) {
         if(team.equals(TeamId.TEAM_1)) {
             return gamePointsT1;
@@ -49,6 +79,12 @@ public final class ScoreBean {
         }
     }
     
+    /**
+     * @Brief It is a public getter for the totalPoints property of the chosen team.
+     *
+     * @param team the TeamId of a team
+     * @return the totalPoints property of the chosen team
+    */
     public ReadOnlyIntegerProperty totalPointsProperty(TeamId team) {
         if(team.equals(TeamId.TEAM_1)) {
             return totalPointsT1;
@@ -58,11 +94,24 @@ public final class ScoreBean {
         }
     }
     
+    
+    /**
+     * @Brief It is a public getter for the winningTeam property.
+     *
+     * @return the winningTeam property
+    */
     public ReadOnlyObjectProperty<TeamId> winningTeamProperty() {
         return winningTeam;
     }
     
     
+    /**
+     * @Brief public setter for the turnPoints given the new turnPoints(int) 
+     * and the chosen team.
+     *
+     * @param team the TeamId of the chosen team
+     * @param newTurnPoints an integer
+    */
     public void setTurnPoints(TeamId team, int newTurnPoints) {
         if(team.equals(TeamId.TEAM_1)) {
             turnPointsT1 = new SimpleIntegerProperty(newTurnPoints);
@@ -72,6 +121,13 @@ public final class ScoreBean {
         }
       }
     
+    /**
+     * @Brief public setter for the gamePoints given the new gamePoints(int) 
+     * and the chosen team.
+     *
+     * @param team the TeamId of the chosen team
+     * @param newGamePoints an integer
+    */
     public void setGamePoints(TeamId team, int newGamePoints) {
         if(team.equals(TeamId.TEAM_1)) {
             gamePointsT1 = new SimpleIntegerProperty(newGamePoints);
@@ -81,6 +137,13 @@ public final class ScoreBean {
         }
       }
     
+    /**
+     * @Brief public setter for the totalPoints given the new totalPoints(int) 
+     * and the chosen team.
+     *
+     * @param team the TeamId of the chosen team
+     * @param newTotalPoints an integer
+    */
     public void setTotalPoints(TeamId team, int newTotalPoints) {
         if(team.equals(TeamId.TEAM_1)) {
             totalPointsT1 = new SimpleIntegerProperty(newTotalPoints);
@@ -91,6 +154,12 @@ public final class ScoreBean {
       }
     
     
+    
+    /**
+     * @Brief public setter for the chosen winning team.
+     *
+     * @param team the TeamId of the winning team
+    */
     public void setWinningTeam(TeamId team) {
             winningTeam = new SimpleObjectProperty<TeamId>(team);
       }
