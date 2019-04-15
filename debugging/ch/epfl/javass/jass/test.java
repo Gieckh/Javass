33ea5d0a842328eb10d00df29a5bce6d038a3229
Scarigli <antoine.scardigli@epfl.ch>
2019-04-15 13:51:09 +0200

    better hand bean

--- debugging/ch/epfl/javass/jass/test.java
+++ debugging/ch/epfl/javass/jass/test.java
@@ -13,24 +13,45 @@ import javafx.collections.SetChangeListener;
 //this is  a test, thus no need of javadoc
 public class test {
     public static void main(String[] args) throws Base64DecodingException {
-        HandBean hb = new HandBean();
-        SetChangeListener<Card> listener = e -> System.out.println(e);
-        hb.playableCards().addListener(listener);
+//        HandBean hb = new HandBean();
+//        SetChangeListener<Card> listener = e -> System.out.println(e);
+//        hb.playableCards().addListener(listener);
+//
+//        CardSet h = CardSet.EMPTY
+//          .add(Card.of(Color.SPADE, Rank.SIX))
+//          .add(Card.of(Color.SPADE, Rank.NINE))
+//          .add(Card.of(Color.SPADE, Rank.JACK))
+//          .add(Card.of(Color.HEART, Rank.SEVEN))
+//          .add(Card.of(Color.HEART, Rank.ACE))
+//          .add(Card.of(Color.DIAMOND, Rank.KING))
+//          .add(Card.of(Color.DIAMOND, Rank.ACE))
+//          .add(Card.of(Color.CLUB, Rank.TEN))
+//          .add(Card.of(Color.CLUB, Rank.QUEEN));
+//        hb.setPlayableCards(h);
+//        while (! h.isEmpty()) {
+//          h = h.remove(h.get(0));
+//          hb.setPlayableCards(h);
+//        }
+//    }
 
-        CardSet h = CardSet.EMPTY
-          .add(Card.of(Color.SPADE, Rank.SIX))
-          .add(Card.of(Color.SPADE, Rank.NINE))
-          .add(Card.of(Color.SPADE, Rank.JACK))
-          .add(Card.of(Color.HEART, Rank.SEVEN))
-          .add(Card.of(Color.HEART, Rank.ACE))
-          .add(Card.of(Color.DIAMOND, Rank.KING))
-          .add(Card.of(Color.DIAMOND, Rank.ACE))
-          .add(Card.of(Color.CLUB, Rank.TEN))
-          .add(Card.of(Color.CLUB, Rank.QUEEN));
-        hb.setPlayableCards(h);
-        while (! h.isEmpty()) {
-          h = h.remove(h.get(0));
-          hb.setPlayableCards(h);
-        }
+    HandBean hb = new HandBean();
+   ListChangeListener<Card> listener = e -> System.out.println(e);
+    hb.hand().addListener(listener);
+
+    CardSet h = CardSet.EMPTY
+      .add(Card.of(Color.SPADE, Rank.SIX))
+      .add(Card.of(Color.SPADE, Rank.NINE))
+      .add(Card.of(Color.SPADE, Rank.JACK))
+      .add(Card.of(Color.HEART, Rank.SEVEN))
+      .add(Card.of(Color.HEART, Rank.ACE))
+      .add(Card.of(Color.DIAMOND, Rank.KING))
+      .add(Card.of(Color.DIAMOND, Rank.ACE))
+      .add(Card.of(Color.CLUB, Rank.TEN))
+      .add(Card.of(Color.CLUB, Rank.QUEEN));
+    hb.setHand(h);
+    while (! h.isEmpty()) {
+      h = h.remove(h.get(0));
+      hb.setHand(h);
     }
+}
 }
