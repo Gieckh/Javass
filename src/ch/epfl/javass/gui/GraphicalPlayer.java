cc8520a413f29c3b474b28faffdf96ce46fc6f5a
Scarigli <antoine.scardigli@epfl.ch>
2019-04-15 14:27:08 +0200

    .

--- src/ch/epfl/javass/gui/GraphicalPlayer.java
+++ src/ch/epfl/javass/gui/GraphicalPlayer.java
@@ -1,15 +1,46 @@
 package ch.epfl.javass.gui;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import ch.epfl.javass.jass.PlayerId;
+
 public class GraphicalPlayer {
     /** ============================================== **/
     /** ==============    ATTRIBUTES    ============== **/
     /** ============================================== **/
 
+    PlayerId thisId;
+    Map<PlayerId, String> playerNames;
+    ScoreBean score;
+    TrickBean trick; 
+        
+        
     /** ============================================== **/
     /** ==============   CONSTRUCTORS   ============== **/
     /** ============================================== **/
 
+    public GraphicalPlayer(PlayerId thisId , Map<PlayerId, String> playerNames, ScoreBean score, TrickBean trick) {
+        this.thisId = thisId; 
+        this.playerNames = playerNames; 
+        this.score = score; 
+        this.trick =trick;
+            
+    }
+    
+    
     /** ============================================== **/
     /** ===============    METHODS    ================ **/
     /** ============================================== **/
+    
+
+    private void createScorePane() {
+        
+    }
+    private void createTrickPane() {
+        
+    }
+    private void createVictoryPanes() {
+        
+    }
 }
