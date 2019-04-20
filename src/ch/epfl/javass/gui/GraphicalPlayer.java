8eb30cd794d5fd91f414ca61506010f66af6f9a7
Scarigli <antoine.scardigli@epfl.ch>
2019-04-20 11:58:01 +0200

    .

--- src/ch/epfl/javass/gui/GraphicalPlayer.java
+++ src/ch/epfl/javass/gui/GraphicalPlayer.java
@@ -1,8 +1,15 @@
 package ch.epfl.javass.gui;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.sun.javafx.collections.UnmodifiableObservableMap;
+
+import ch.epfl.javass.jass.Card;
+import ch.epfl.javass.jass.Card.Color;
+import ch.epfl.javass.jass.Card.Rank;
+import ch.epfl.javass.jass.PackedCard;
 import ch.epfl.javass.jass.PlayerId;import ch.epfl.javass.jass.TeamId;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.StringExpression;
@@ -13,8 +20,14 @@ import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.control.Label;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 public class GraphicalPlayer {
@@ -26,7 +39,12 @@ public class GraphicalPlayer {
     Map<PlayerId, String> playerNames;
     ScoreBean score;
     TrickBean trick; 
-        
+    private static Map<Card,Image> cardImpage240 = GraphicalPlayer.cardImage240();
+    private static Map<Card,Image> cardImpage160 = GraphicalPlayer.cardImage160();
+    private static Map<Color,Image> trumpImage = GraphicalPlayer.trumpImage();
+
+
+    
         
     /** ============================================== **/
     /** ==============   CONSTRUCTORS   ============== **/
@@ -48,7 +66,43 @@ public class GraphicalPlayer {
     /** ===============    METHODS    ================ **/
     /** ============================================== **/
     
-   // private Image
+    @SuppressWarnings("unchecked")
+    private static Map<Card,Image> cardImage160() {
+        Map<Card,Image> map = new HashMap<>();
+        for(int i = 0 ; i<Color.ALL.size(); ++i) {
+            for(int j = 0 ; j < Rank.ALL.size(); ++j) {
+                String s = "/"+"card_"+i+"_"+j+"_"+160+".png";
+                map.put(Card.of(Color.ALL.get(i),Rank.ALL.get(j)), new Image(s));
+            }
+        }
+        //unsure transtypage askip
+        return  Collections.unmodifiableMap(new HashMap(map));
+    }
+    
+    @SuppressWarnings("unchecked")
+    private static Map<Card,Image> cardImage240() {
+        Map<Card,Image> map = new HashMap<>();
+        for(int i = 0 ; i<Color.ALL.size(); ++i) {
+            for(int j = 0 ; j < Rank.ALL.size(); ++j) {
+                String s = "/"+"card_"+i+"_"+j+"_"+240+".png";
+                map.put(Card.of(Color.ALL.get(i),Rank.ALL.get(j)), new Image(s));
+            }
+        }
+        //unsure transtypage askip
+        return  Collections.unmodifiableMap(new HashMap(map));
+    }
+    
+    @SuppressWarnings("unchecked")
+    private static Map<Color,Image> trumpImage() {
+        Map<Color,Image> map = new HashMap<>();
+        for(int i = 0 ; i<Color.ALL.size(); ++i) {
+            String s = "/"+"card_"+i+"_"+".png";
+            map.put(Color.ALL.get(i),new Image(s));
+        }
+        
+        //unsure transtypage askip
+        return  Collections.unmodifiableMap(new HashMap(map));
+    }
 
     private void createScorePane() {
         Label namesOfTeam1 = new Label(playerNames.get(PlayerId.PLAYER_1).toString()
@@ -98,10 +152,31 @@ public class GraphicalPlayer {
         grid.setStyle("-fx-font: 16 Optima;\n" + 
                     "-fx-background-color: lightgray;\n" + 
                     "-fx-padding: 5px;\n" + 
-                    "-fx-alignment: center;");
+                    "-fx-alignment: center;");        
     }
     private void createTrickPane() {
+        StackPane stackpane = new StackPane(new Rectangle(120, 180));
+        stackpane.setStyle("-fx-arc-width: 20;\n" + 
+                "-fx-arc-height: 20;\n" + 
+                "-fx-fill: transparent;\n" + 
+                "-fx-stroke: lightpink;\n" + 
+                "-fx-stroke-width: 5;\n" + 
+                "-fx-opacity: 0.5;");
+        stackpane.setEffect(new GaussianBlur(4));
         
+        Text textForCouple1 = 
+        textForCouple1.setStyle("-fx-font: 14 Optima;");      
+        ImageView ImageForCouple1 =
+        VBox couple1 = 
+        GridPane grid = new GridPane();
+        
+        
+        grid.setStyle("-fx-background-color: whitesmoke;\n" + 
+                "-fx-padding: 5px;\n" + 
+                "-fx-border-width: 3px 0px;\n" + 
+                "-fx-border-style: solid;\n" + 
+                "-fx-border-color: gray;\n" + 
+                "-fx-alignment: center;");
     }
     private void createVictoryPanes(TeamId winningTeam) {
         
