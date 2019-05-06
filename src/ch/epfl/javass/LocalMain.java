81a1f0a3be84879eddd0c96d6a6fa4792a59276e
Scarigli <antoine.scardigli@epfl.ch>
2019-05-06 14:52:27 +0200

    tache 11

--- src/ch/epfl/javass/LocalMain.java
+++ src/ch/epfl/javass/LocalMain.java
@@ -4,14 +4,28 @@ import java.util.List;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import static javafx.application.Application.Parameters;
 
 public class LocalMain extends Application {
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        List<String> args = getParameters().getRaw();
+        List<String> args2 = getParameters().getRaw();
+
+        if(args2.size()!=4 || args2.size() !=5) {
+            System.out.println("Utilisation: java ch.epfl.javass.LocalMain <j1>…<j4> [<graine>]\n" + 
+                    "où :\n" + 
+                    "<jn> spécifie le joueur n, ainsi:\n" + 
+                    "  h:<nom>  un joueur humain nommé <nom> \n"+
+                    "  s:<nom>:<itérations>  un joueur simulé nommé <nom> avec <itérations> itérations \n" +
+                    "  r:<nom>:<IP-Host>  un joueur humain à distance nommé <nom> et avec l'adresse IP <IP-Host> \n"+
+                    "  <graine> la graine génératrice des autres graines utilisées partout dans le jeu \n"+
+                    " où les noms sont optionnels (par defaut : Aline, Bastien, Colette et David dans l'ordre ) \n " +
+                    " où les itérations sont optionnelles (par defaut : 10 000 )\n " +
+                    " où les IP-Host sont optionnels (par defaut : localhost )\n "+
+                    " où graine est optionnelle et est forcément en 5ème position \n ");
+            System.exit(1);
+        }
     }
 
     public static void main(String[] args) {
