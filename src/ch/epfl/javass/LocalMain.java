ddaa13f34b34767947f0d43781f1be7d270d53ef
Scarigli <antoine.scardigli@epfl.ch>
2019-05-06 14:21:00 +0200

    .

--- src/ch/epfl/javass/LocalMain.java
+++ src/ch/epfl/javass/LocalMain.java
@@ -1,24 +1,16 @@
 package ch.epfl.javass;
 
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class LocalMain extends Application {
-    /** ============================================== **/
-    /** ==============    ATTRIBUTES    ============== **/
-    /** ============================================== **/
 
-    /** ============================================== **/
-    /** ==============   CONSTRUCTORS   ============== **/
-    /** ============================================== **/
-
-    /** ============================================== **/
-    /** ===============    METHODS    ================ **/
-    /** ============================================== **/
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        List<String> args2 = Application.getParameters().getRaw();
     }
 
     public static void main(String[] args) {
