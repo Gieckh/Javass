f8c28cfec232885c181980ad6957f4d2c32cdcf0
Scarigli <antoine.scardigli@epfl.ch>
2019-05-17 10:41:21 +0200

    smart reductor of set is seeming being working

--- debugging/ch/epfl/javass/jass/test.java
+++ debugging/ch/epfl/javass/jass/test.java
@@ -1,5 +1,6 @@
 package ch.epfl.javass.jass;
 
+import bonus.JassReductorOfSet;
 import ch.epfl.javass.gui.HandBean;
 import ch.epfl.javass.jass.Card.Color;
 import ch.epfl.javass.jass.Card.Rank;
@@ -10,20 +11,12 @@ import javafx.collections.ListChangeListener;
 //this is  a test, thus no need of javadoc
 public class test {
     public static void main(String[] args) {
-        System.out.printf("oui oui %d oui %d", 10, 12);
-
-
-//        ObjectProperty<String> p1 = new SimpleObjectProperty<>();
-//        ObjectProperty<String> p2 = new SimpleObjectProperty<>();
-//
-//        p1.addListener((o, oV, nV) -> System.out.println(nV));
-//        p1.addListener((o, oV, nV) -> System.out.println(oV));
-//        p1.addListener((o, oV, nV) -> System.out.println(""));
-//
-//        p2.set("hello");                // n'affiche rien
-//        System.out.println("--- binding p1 to p2");
-//        p1.bindBidirectional(p2);       // affiche "hello"
-//        p2.set("world");                // affiche "world"
-//        p1.set("bonjour");
+       Trick coucou = Trick.firstEmpty(Color.CLUB, PlayerId.PLAYER_1);
+       coucou = coucou.withAddedCard(Card.of(Color.DIAMOND, Rank.KING));
+       coucou = coucou.withAddedCard(Card.of(Color.HEART,Rank.ACE));
+       coucou= coucou.withAddedCard(Card.of(Color.CLUB, Rank.QUEEN));
+       coucou = coucou.withAddedCard(Card.of(Color.CLUB, Rank.SIX));
+       System.out.println(JassReductorOfSet.CardsThePlayerHavnt(coucou));
+       
     }
 }
