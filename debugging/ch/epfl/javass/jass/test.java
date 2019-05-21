499d7de23afd239ccd37fcce5539737badb7785d
Gieckh <marin.nguyen@gmail.com>
2019-05-21 15:20:27 +0200

    .

--- debugging/ch/epfl/javass/jass/test.java
+++ debugging/ch/epfl/javass/jass/test.java
@@ -1,16 +1,13 @@
 package ch.epfl.javass.jass;
 
-import bonus.JassReductorOfSet;
-import ch.epfl.javass.PlayerType;
-
 import java.util.Arrays;
 import java.util.List;
 
 //this is  a test, thus no need of javadoc
 public class test {
     public static void main(String[] args) {
-        String s = "a::b";
-        List<String> ls = Arrays.asList(s.split(":"));
-        System.out.println(ls);
+        List<String> l = Arrays.asList("r", "k");
+        l.remove("ok");
+
     }
 }
